0ad17075e5e2e8c5fcd49c5ceaaa8fde19a17064
Sushant Mehta <mehtasushant05@gmail.com>
2018-07-26 10:51:59 +0530

    Led testing -1

---
+++ led_test_1/led_test_1.ino
@@ -0,0 +1,63 @@
+int count = 0;
+int led[4];
+int i;
+int j = 4;
+for(i=0;i<4;i++)
+{
+  led[i] = j;
+  j++;
+}
+
+void setup() {
+  pinMode(led[0], OUTPUT);
+  pinMode(led[1], OUTPUT);
+  pinMode(led[2], OUTPUT);
+  pinMode(led[3], OUTPUT);
+}
+
+void loop() {
+ 
+  if(count==0)
+ {
+  digitalWrite(led[0], LOW);
+  digitalWrite(led[1], LOW);
+  digitalWrite(led[2], LOW);
+  digitalWrite(led[3], LOW);
+ }
+ else if(count>0 && count<!=7)
+ {
+  digitalWrite(led[0], HIGH);
+  digitalWrite(led[1], LOW);
+  digitalWrite(led[2], LOW);
+  digitalWrite(led[3], LOW);
+ }
+ else if(count>7 && count<=14)
+ {
+  
+  digitalWrite(led[0], HIGH);
+  digitalWrite(led[1], HIGH);
+  digitalWrite(led[2], LOW);
+  digitalWrite(led[3], LOW);
+ }
+ 
+ else if(count>14 && count<=21)
+ {
+  
+  digitalWrite(led[0], HIGH);
+  digitalWrite(led[1], HIGH);
+  digitalWrite(led[2], HIGH);
+  digitalWrite(led[3], LOW);
+ }
+ 
+ else if(count>21 && count<=28)
+ {
+  
+  digitalWrite(led[0], HIGH);
+  digitalWrite(led[1], HIGH);
+  digitalWrite(led[2], HIGH);
+  digitalWrite(led[3], HIGH);
+ }
+
+count ++;
+delay(2000);
+}
